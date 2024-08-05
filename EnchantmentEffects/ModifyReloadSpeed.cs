b84d00ec82eea099ad8f4e9cfbf651252dd7695e
nutnnut <nut.euangkanakul@gmail.com>
2024-08-05 18:22:53 +0700

    reload speed

---
+++ EnchantmentEffects/ModifyReloadSpeed.cs
@@ -0,0 +1,26 @@
+﻿using ItemDataManager;
+using JetBrains.Annotations;
+using kg.ValheimEnchantmentSystem.Misc;
+using static kg.ValheimEnchantmentSystem.Enchantment_Core;
+
+namespace kg.ValheimEnchantmentSystem.EnchantmentEffects
+{
+    [HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetWeaponLoadingTime))]
+    [ClientOnlyPatch]
+    public static class ItemWeaponLoadingTime_Patch
+    {
+        [UsedImplicitly]
+        private static void Postfix(ItemDrop.ItemData __instance, ref float __result)
+        {
+            Player player = Player.m_localPlayer;
+
+            float totalAttackSpeedBonus = 0;
+            foreach (var en in player.EquippedEnchantments())
+            {
+                if (en.Stats is { } stats) totalAttackSpeedBonus += stats.attack_speed / 100f;
+            }
+
+            __result /= 1.0f + totalAttackSpeedBonus;
+        }
+    }
+}
