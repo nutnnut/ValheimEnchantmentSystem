de90ccfb9af163b4bf6535db01716c69b08cb887
Sirinut Euaungkanakul <sirinut.e@siamhos.com>
2024-08-21 16:42:37 +0700

    fix

--- Enchantment_VFX.cs
+++ Enchantment_VFX.cs
@@ -87,12 +87,13 @@ public static class Enchantment_VFX
             l.intensity *= isArmor ? 0.25f * c.a : 1.0f * c.a;
             l.range = 9f;
         }
+        int original_variant = variant;
      
         List<Renderer> renderers = item.GetComponentsInChildren<SkinnedMeshRenderer>(true).Cast<Renderer>().Concat(item.GetComponentsInChildren<MeshRenderer>(true)).ToList();
         foreach (Renderer renderer in renderers)
         {
             List<Material> list = renderer.sharedMaterials.ToList();
-            if (isArmor && variant == 4) variant = 1;
+            if (isArmor && original_variant == 4) variant = 1;
             list.Add(VFXs[variant]);
             renderer.sharedMaterials = list.ToArray();
             // if (isArmor) continue;
@@ -131,9 +132,9 @@ public static class Enchantment_VFX
             if (material.name.Contains("Enchantment_VFX_Mat"))
             {
                 material.SetColor(TintColor, c * INTENSITY[variant]);
-                float speedMultiplier = variant == 4 ? 0.1f : 0.5f;
+                float speedMultiplier = variant == 4 ? 0.2f : 0.5f;
                 material.SetVector("_TimeScale", material.GetVector("_TimeScale") * speedMultiplier);
-                if (isArmor && variant == 4) material.mainTextureScale *= 0.25f;
+                if (isArmor && original_variant == 4) material.mainTextureScale *= 0.25f;
             }
         }
     }
